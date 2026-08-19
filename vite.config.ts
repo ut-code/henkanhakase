ee8feba04168a34c430893e9ec8d23631c225a38
faithia-anastasia <211831874+faithia-anastasia@users.noreply.github.com>
2026-08-19 11:49:35 +0900

    introduce biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [
-      react(),
-      tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
